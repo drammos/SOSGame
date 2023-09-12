1a97e8321e260e60b806655b0d8f9789f10e2a73
Dimitrios Rammos <61520191+drammos@users.noreply.github.com>
2023-09-12 17:12:17 +0300

    Add panel grid

--- SOS/SOS/StartGame.xaml.cs
+++ SOS/SOS/StartGame.xaml.cs
@@ -1,9 +1,38 @@
-namespace SOS;
-
-public partial class StartGame : ContentPage
+namespace SOS
 {
-	public StartGame()
-	{
-		InitializeComponent();
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StartGame : ContentPage
+    {
+        public StartGame()
+        {
+            InitializeComponent();
+            BindingContext = new SOSViewModel(); // Συνδέστε την σελίδα με το ViewModel
+        }
+
+
+    }
+    public class SOSViewModel
+    {
+        public List<SOSItem> Board { get; set; }
+
+        public SOSViewModel()
+        {
+            // Δημιουργήστε τον πίνακα 8x8 με αρχικές τιμές
+            Board = new List<SOSItem>();
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Board.Add(new SOSItem { Symbol = "" });
+                }
+            }
+        }
+    }
+
+    public class SOSItem
+    {
+        public string Symbol { get; set; }
+    }
+
+
 }
